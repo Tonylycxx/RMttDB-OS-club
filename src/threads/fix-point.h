8767ecb7fe61aa0f71a824c9792e65f5ff9ca931
Tonylycxx <2290549567@qq.com>
2020-10-28 20:10:41 +0800

    completed fix-point.h

--- src/threads/fix-point.h
+++ src/threads/fix-point.h
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 #define FIX_POINT_FLAG_P 17
 #define FIX_POINT_FLAG_Q 31 - FIX_POINT_FLAG_P
 
@@ -12,3 +14,9 @@
 
 #define add_fixed_int(x, n) x + n *FIX_POINT_FLAG_F
 #define sub_fixed_int(x, n) x - n *FIX_POINT_FLAG_F
+
+#define mul_fixed_fixed(x, y) ((int64_t)x) * y / FIX_POINT_FLAG_F
+#define mul_fixed_int(x, n) x *n
+
+#define div_fixed_fixed(x, y) ((int64_t)x) * f / y
+#define div_fixed_int(x, n) x / n
