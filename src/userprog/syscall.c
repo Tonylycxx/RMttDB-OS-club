b5b264675b1914319476beaaf3f29af12dbda892
luan6ma <1537011738@qq.com>
2020-11-26 06:02:53 -0800

    syscall-handler

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,6 +6,12 @@
 
 static void syscall_handler (struct intr_frame *);
 
+int *
+getargu(void *esp, int argument_index)
+{
+  return (int *)esp + argument_index + 1;
+}
+
 void
 syscall_init (void) 
 {
@@ -15,18 +21,61 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f) 
 {
-  // printf ("system call!\n");
   int status = *(int *)f->esp;
 
   switch(status)
   {
-    case SYS_EXIT:
-      exit(*((int *)f->esp + 1));
+	  case SYS_HALT:
+      halt ();
       break;
 
-    case SYS_WRITE:
-      write(f, *((int *)(f->esp) + 1), (const void *)*((int *)(f->esp) + 2), (unsigned)*((int *)(f->esp) + 3));
+    case SYS_EXIT:
+      exit ((int)(*getargu(f->esp, 0)));
       break;
+
+    // case SYS_EXEC:
+    //   exec ((char *)(*getargu(f->esp, 0)));
+    //   break;
+
+    // case SYS_WAIT:
+    //   wait ((tid_t)(*getargu(f->esp, 0)));
+    //   break;
+
+    // case SYS_CREATE:
+    //   create ((char *)(*getargu(f->esp, 0)), (unsigned)(*getargu(f->esp, 1)));
+    //   break;
+
+    // case SYS_REMOVE:
+    //   remove ((char *)(*getargu(f->esp, 0)));
+    //   break;
+
+    // case SYS_OPEN:
+    //   open ((char *)(*getargu(f->esp, 0)));
+    //   break;
+
+    // case SYS_FILESIZE:
+    //   filesize ((int)(*getargu(f->esp, 0)));
+    //   break;
+
+    // case SYS_READ:
+    //   read ((int)(*getargu(f->esp, 0)), (void *)(*getargu(f->esp, 1)), (unsigned)(*getargu(f->esp, 2)));
+    //   break;
+
+    case SYS_WRITE:
+      write (f, (int)(*getargu(f->esp, 0)), (void *)(*getargu(f->esp, 1)), (unsigned)(*getargu(f->esp, 2)));
+      break;
+
+    // case SYS_SEEK:
+    //   seek ((int)(*getargu(f->esp, 0)), (unsigned)(*getargu(f->esp, 1)));
+    //   break;
+
+    // case SYS_TELL:
+    //   tell ((int)(*getargu(f->esp, 0)));
+    //   break;
+
+    // case SYS_CLOSE:
+    //   close ((int)(*getargu(f->esp, 0)));
+    //   break;
     
     default:
       thread_exit ();
@@ -34,22 +83,85 @@ syscall_handler (struct intr_frame *f)
   }
 }
 
-void exit(int ret)
+void
+halt (void)
 {
-  thread_current()->ret_val = ret;
+  shutdown_power_off ();
+}
+
+void
+exit (int status)
+{
+  thread_current()->ret_val = status;
   thread_exit ();
 }
 
-int write(struct intr_frame *f, int fd, const void *buffer, unsigned size)
+// tid_t
+// exec (const char *cmd_line)
+// {
+  
+// }
+
+// int
+// wait (tid_t tid)
+// {
+
+// }
+
+// bool
+// create (const char *file, unsigned initial_size)
+// {
+
+// }
+
+// bool
+// remove (const char *file)
+// {
+
+// }
+
+// int
+// open (const char *file)
+// {
+
+// }
+
+// int
+// filesize (int fd)
+// {
+
+// }
+
+// int
+// read (int fd, void *buffer, unsigned size)
+// {
+
+// }
+
+int
+write (struct intr_frame *f, int fd, const void *buffer, unsigned size)
 {
   if(fd == STDOUT_FILENO) 
   {
     putbuf((const char *)buffer, size);
     f->eax = size;
-  }
-  else
-  {
-    
-  }
-  
+  }  
 }
+
+// void
+// seek (int fd, unsigned position)
+// {
+
+// }
+
+// unsigned
+// tell (int fd)
+// {
+
+// }
+
+// void
+// close (int fd)
+// {
+
+// }
