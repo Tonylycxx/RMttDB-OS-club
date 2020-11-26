6889b47d1830885aa6a26c1fd792dc0e83700504
Tonylycxx <2290549567@qq.com>
2020-11-26 20:53:46 +0800

    Argument Pass little changes v1.0

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -3,6 +3,11 @@
 
 #include "threads/thread.h"
 
+struct pcb {
+    char *exec_name;
+    char *cmd_line;
+};
+
 tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
