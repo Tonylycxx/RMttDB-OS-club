6ad85f9d1f67827871c1f6e168db4876318f6763
luan6ma <1537011738@qq.com>
2020-11-28 23:54:12 -0800

    seek tell

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -24,6 +24,16 @@ void check_valid_addr(const void *ptr_to_check)
     exit(-1);
 }
 
+bool is_valid_addr(const void *ptr_to_check)
+{
+  struct thread *cur = thread_current();
+  if (!ptr_to_check || ptr_to_check <= 0x08048000 || !is_user_vaddr(ptr_to_check))
+    return 0;
+  else if (pagedir_get_page(cur->pagedir, ptr_to_check) == NULL)
+    return 0;
+  return 1;
+}
+
 void getargu(void *esp, int *arg, int argument_index)
 {
   int i;
@@ -34,6 +44,7 @@ void getargu(void *esp, int *arg, int argument_index)
     arg[i] = *res;
   }
 }
+
 static void
 syscall_handler(struct intr_frame *f)
 {
@@ -78,12 +89,16 @@ syscall_handler(struct intr_frame *f)
       create(f, (char *)arg[0], (unsigned)arg[1]);
       break;
 
-      // case SYS_REMOVE:
-      //   remove ((char *)(*getargu(f->esp, 0)));
-      //   break;
+    case SYS_REMOVE:
+      getargu(f->esp, &arg[0], 1);
+      phys_page_ptr = pagedir_get_page(thread_current()->pagedir, arg[0]);
+      if (!phys_page_ptr)
+        exit(-1);
+      arg[0] = phys_page_ptr;
+      remove (f, (char *)arg[0]);
+      break;
 
     case SYS_OPEN:
-      //f->eax = ((char *)(*getargu(f->esp, 0)));
       getargu(f->esp, &arg[0], 1);
       phys_page_ptr = pagedir_get_page(thread_current()->pagedir, arg[0]);
       if (!phys_page_ptr)
@@ -115,13 +130,23 @@ syscall_handler(struct intr_frame *f)
       write(f, (int)arg[0], (void *)arg[1], (unsigned)arg[2]);
       break;
 
-      // case SYS_SEEK:
-      //   seek ((int)(*getargu(f->esp, 0)), (unsigned)(*getargu(f->esp, 1)));
-      //   break;
+    case SYS_SEEK:
+      getargu(f->esp, &arg[0], 2);
+      phys_page_ptr = pagedir_get_page(thread_current()->pagedir, arg[1]);
+      if (!phys_page_ptr)
+        exit(-1);
+      arg[1] = phys_page_ptr;
+      seek (f, (int)arg[0], (unsigned)arg[1]);
+      break;
 
-      // case SYS_TELL:
-      //   tell ((int)(*getargu(f->esp, 0)));
-      //   break;
+    case SYS_TELL:
+      getargu(f->esp, &arg[0], 1);
+      phys_page_ptr = pagedir_get_page(thread_current()->pagedir, arg[1]);
+      if (!phys_page_ptr)
+        exit(-1);
+      arg[1] = phys_page_ptr;
+      tell (f, (int)arg[0]);
+      break;
 
     case SYS_CLOSE:
       getargu(f->esp, &arg[0], 1);
@@ -134,23 +159,27 @@ syscall_handler(struct intr_frame *f)
   }
 }
 
-void halt(void)
+void
+halt(void)
 {
   shutdown_power_off();
 }
 
-void exit(int status)
+void
+exit(int status)
 {
   thread_current()->ret_val = status;
   thread_exit();
 }
 
-tid_t exec(struct intr_frame *f, const char *cmd_line)
+void
+exec(struct intr_frame *f, const char *cmd_line)
 {
   f->eax = process_execute(cmd_line);
 }
 
-int wait(struct intr_frame *f, tid_t tid)
+void
+wait(struct intr_frame *f, tid_t tid)
 {
   f->eax = process_wait(tid);
 }
@@ -161,13 +190,14 @@ create (struct intr_frame *f, const char *file, unsigned initial_size)
   f->eax = filesys_create(file, initial_size);
 }
 
-// bool
-// remove (const char *file)
-// {
+void
+remove (struct intr_frame *f, const char *file)
+{
+  f->eax = filesys_remove(file);
+}
 
-// }
-
-int open(struct intr_frame *f, const char *file_name)
+void
+open(struct intr_frame *f, const char *file_name)
 {
   if (file_name == NULL)
   {
@@ -189,9 +219,10 @@ int open(struct intr_frame *f, const char *file_name)
   return fd;
 }
 
-void filesize(struct intr_frame *f, int fd)
+void
+filesize(struct intr_frame *f, int fd)
 {
-  if(fd == 0||fd == 1)
+  if(fd == STDIN_FILENO||fd == STDOUT_FILENO)
   {
     f->eax = -1;
     return;
@@ -213,6 +244,7 @@ void filesize(struct intr_frame *f, int fd)
 void
 read (struct intr_frame *f, int fd, void *buffer, unsigned size)
 {
+  printf("%x", (unsigned)buffer);
   if (fd == STDIN_FILENO)
   {
     f->eax = (int) input_getc();
@@ -229,7 +261,7 @@ read (struct intr_frame *f, int fd, void *buffer, unsigned size)
        e = list_next(e))
   {
     struct opened_file *op_file = list_entry(e, struct opened_file, elem);
-    if(op_file->fd == fd)
+    if(fd == op_file->fd)
     {
       f->eax = file_read(op_file->f, buffer, size);
       return;
@@ -238,7 +270,8 @@ read (struct intr_frame *f, int fd, void *buffer, unsigned size)
   f->eax = 0;
 }
 
-void write(struct intr_frame *f, int fd, const void *buffer, unsigned size)
+void
+write(struct intr_frame *f, int fd, const void *buffer, unsigned size)
 {
   if (fd == STDOUT_FILENO)
   {
@@ -265,17 +298,48 @@ void write(struct intr_frame *f, int fd, const void *buffer, unsigned size)
   f->eax = 0;
 }
 
-// void
-// seek (int fd, unsigned position)
-// {
+void
+seek (struct intr_frame *f, int fd, unsigned position)
+{
+  if (fd == STDIN_FILENO || fd == STDOUT_FILENO)
+  {
+    return;
+  }
 
-// }
+  struct list_elem *e;
+  for (e = list_begin(&thread_current()->opened_files); e != list_end(&thread_current()->opened_files);
+       e = list_next(e))
+  {
+    struct opened_file *op_file = list_entry(e, struct opened_file, elem);
+    if(fd == op_file->fd)
+    {
+      file_seek(op_file->f, position);
+      return;
+    }
+  }
+}
 
-// unsigned
-// tell (int fd)
-// {
+void
+tell (struct intr_frame *f, int fd)
+{
+  if (fd == STDIN_FILENO || fd == STDOUT_FILENO)
+  {
+    f->eax = -1;
+    return;
+  }
 
-// }
+  struct list_elem *e;
+  for (e = list_begin(&thread_current()->opened_files); e != list_end(&thread_current()->opened_files);
+       e = list_next(e))
+  {
+    struct opened_file *op_file = list_entry(e, struct opened_file, elem);
+    if(fd == op_file->fd)
+    {
+      f->eax = file_tell(op_file->f);
+      return;
+    }
+  }
+}
 
 void
 close (struct intr_frame *f, int fd)
